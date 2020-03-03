8993d2f90c18a23bc8ccd32579301a7316380bbb
oursys <admin@oursys.com.br>
2020-03-03 03:04:33 -0300

    Arrumando o arquivo de configuracao

--- public/config/consts.js
+++ public/config/consts.js
@@ -4,10 +4,10 @@
         version: '1.0',
         owner: 'Oursys',
         year: '2019',
-         //apiUrl: 'https://api-qrponto.oursys.com.br',
-         //oapiUrl: 'https://api-qrponto.oursys.com.br/oapi',
-        apiUrl: 'http://localhost:4040',
-        oapiUrl: 'http://localhost:4040/oapi',
+        apiUrl: 'https://api-qrponto.oursys.com.br',
+        oapiUrl: 'https://api-qrponto.oursys.com.br/oapi',
+        //apiUrl: 'http://localhost:4040',
+        //oapiUrl: 'http://localhost:4040/oapi',
         buscaCNPJ: 'https://www.receitaws.com.br/v1/cnpj',
         userKey: 'app_qr_ponto_user'
     }).run(['$rootScope', 'consts', function($rootScope, consts) {
